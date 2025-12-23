857a278b8e5a930efccbb98c0ae53b870b379f70
Deepesh Chhetri <chhetri.deepesh95@gmail.com>
2025-12-23 14:41:24 +0800

    next config changes for vercel pdf compatibility test

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-// v15 made this property stable
   serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+
+  // Force Next.js to include the chromium binary payload in the serverless bundle
+  outputFileTracingIncludes: {
+    "/api/report/pdf": [
+      "./node_modules/@sparticuz/chromium/bin/**",
+    ],
+  },
 };
 
 export default nextConfig;
